ec00f470a55b030157d7671c307c787079a986ec
Andrei Makhnach <ahdpyxa88@gmail.com>
2023-10-01 18:49:47 +0300

    fix: fix bugs

--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  let result = [];
-  matrix.forEach(row => {
-    row.forEach(item => {
-      result.push(item);
-    })
-  });
-  return result;}
+    let result = [];
+    let temp = [];
+    if (matrix) {
+      matrix.forEach((row, rowInd) => {
+        temp = [];
+        row.forEach(item => {
+          if (rowInd % 2) {
+            temp.unshift(item);
+          }else{
+            result.push(item);
+          }
+        })
+        result = result.concat(temp);
+      });
+    }
+  return result;
+}
